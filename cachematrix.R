0c3e31954a92795d2af18f5ac798ade857b8c7d3
tbales <pepslove@hotmail.com>
2014-07-27 15:16:23 -0700

    modified cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,34 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         
+        # set inverse variable
         m <- NULL
-        set <- function(y) {
-                x <<- y
+        
+        # set the matrix
+        set <- function(matrix) {
+                x <<- matrix
                 m <<- NULL
         }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        
+        # get the matrix 
+        get <- function() {
+        	x
+        }
+        
+        # set the inverse of the matrix
+        set_inverse <- function(inverse) {
+        	m <<- inverse
+        }	
+        
+        # get the inserve of the matrix
+        get_inverse <- function() {
+        	m
+        }
+        
+        # list internal functions
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 
 }
 
@@ -24,15 +41,25 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         
-                m <- x$getmean()
-	        if(!is.null(m)) {
-	                message("getting cached data")
-	                return(m)
-	        }
-	        data <- x$get()
-	        m <- mean(data, ...)
-	        x$setmean(m)
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse()
+	
+	# or else, retrieve the inverse matrix from the cache
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	
+	# get the original matrix
+	data <- x$get()
+	
+	# calculate the inverse of the matrix using matrix multiplication
+	m <- solve(data %*% data)
+	
+	# assign the inverse to x
+	x$set_inverse(m)
+        
+        # return the matrix
         m
 }
